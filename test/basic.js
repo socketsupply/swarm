d599ba9944c55b63644ca89ea5153445120e8238
Dominic Tarr <dominic.tarr@gmail.com>
2022-07-08 16:01:09 +1200

    test nat discovery

--- test/basic.js
+++ test/basic.js
@@ -16,6 +16,9 @@ var D = '4.4.4.4'
 
 var P = ":100"
 
+function short (id) {
+  return id.substring(0, 8)
+}
 
 tape('3 servers, 1 peer', function (t) {
   var net = new Network()
@@ -39,10 +42,46 @@ tape('3 servers, 1 peer', function (t) {
 
   for(var k in net.subnet) {
     console.log(k, net.subnet[k].data.id)
-    t.equal(net.subnet[k].data.nat, 'easy')
+    var nat = net.subnet[k].data.nat
+    t.ok(nat === 'easy' || nat === 'static', 'peers know their nat is easy or static')
     //every peer should have received pongs from every other peer
     t.deepEqual(peersOf(net.subnet[k]), peer_ids)
   }
+
+  var nats = {}
+
+  for(var k in net.subnet) {
+    console.log(
+      short(net.subnet[k].data.id),
+      net.subnet[k].data.nat
+    )
+    for(var j in net.subnet[k].data.peers) {
+      var nat = net.subnet[k].data.peers[j].nat
+      nats[nat] = (nats[nat] || 0) + 1
+      console.log(
+        short(net.subnet[k].data.id), 
+        short(j),
+        net.subnet[k].data.peers[j].nat
+      )
+    }
+  }
+  console.log(nats)
+  t.notOk(nats.unknown, 'should have decided what the nats are')
+
+  for(var k in net.subnet) {
+    console.log(k, short(net.subnet[k].data.id))
+  }
+
+  //the first 3 peers receive direct pings from D,
+  //so they know they have static nats
+  t.equal(net.subnet[A].data.nat, 'static')
+  t.equal(net.subnet[B].data.nat, 'static')
+  t.equal(net.subnet[C].data.nat, 'static')
+  //4th peer (which initiates gossip) only receives replies
+  //so they do not know for sure that they are static
+  //but they received responses on the same port so they know they are easy at least.
+  t.equal(net.subnet[D].data.nat, 'easy')
+
   //each peer should have sent 3 pings + 3 pongs + peer messages 1, 2, 3
   t.end()
 })
